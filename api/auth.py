a2d57cd164a01503c3b67f7dd109262283d04c7e
JMaylor <joseph.maylor@gmail.com>
2020-09-05 22:03:56 +0100

    fixed unauthorizederror handling

--- api/auth.py
+++ api/auth.py
@@ -1,4 +1,4 @@
-from flask import Response, request
+from flask import Response, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from database.models import User
 from flask_restful import Resource
@@ -39,11 +39,13 @@ class LoginApi(Resource):
 			user = User.objects.get(email=body.get('email'))
 			authorized = user.check_password(body.get('password'))
 			if not authorized:
-				return {'error': 'Email or password invalid'}, 401
+				raise UnauthorizedError
 			expires = datetime.timedelta(days=7)
 			access_token = create_access_token(identity=str(user.id), expires_delta=expires)
 			return {'token': access_token}, 200
-		except (UnauthorizedError, DoesNotExist):
+		except DoesNotExist:
+			raise UnauthorizedError
+		except UnauthorizedError:
 			raise UnauthorizedError
 		except Exception as e:
 			raise InternalServerError
